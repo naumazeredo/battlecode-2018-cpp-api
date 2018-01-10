08923591e986e32e57da3ba9b9afc90decfaf8dc
Naum Azeredo <naumazeredo@gmail.com>
2018-01-10 20:11:07 -0300

    Add GameController

--- bc.hpp
+++ bc.hpp
@@ -8,19 +8,19 @@
  *
  */
 
-// XXX: Update when C API is released (using Python as reference)
-
 #include <vector>
 #include <climits>
 
 #include <bc.h>
 
 // TODO: Think if it's good to save the bc pointer in every class
+// TODO: Add to_bc() on classes to return the stored bc pointer
 
 
 namespace bc {
 
 // Logger
+// TODO: Use bc_has_err and stuff
 #ifdef NDEBUG
 #define log_error(condition, message) ((void)0)
 #else
@@ -88,16 +88,26 @@ public:
   MapLocation(Planet planet, int x, int y) : m_planet { planet }, m_x { x }, m_y { y }
   {}
 
-  MapLocation(bc_MapLocation* map_location) {
+  MapLocation(bc_MapLocation* map_location) : m_map_location { map_location } {
+    log_error(map_location, "Null bc_MapLocation!");
+
     m_planet = bc_MapLocation_planet_get(map_location);
     m_x      = bc_MapLocation_x_get(map_location);
     m_y      = bc_MapLocation_y_get(map_location);
-    delete_bc_MapLocation(map_location);
+  }
+
+  ~MapLocation() {
+    if (m_map_location)
+      delete_bc_MapLocation(m_map_location);
   }
 
   // TODO: Copy/move semantics
 
-  bc_MapLocation* to_bc() const { return new_bc_MapLocation(Planet, x, y); }
+  bc_MapLocation* to_bc() const {
+    if (!m_map_location)
+      m_map_location = new_bc_MapLocation(Planet, x, y);
+    return m_map_location;
+  }
 
   Planet get_planet() const { return m_planet; }
   int get_x() const { return m_x; }
@@ -162,6 +172,8 @@ public:
   // TODO: MapLocation JSON
 
 private:
+  bc_MapLocation* m_map_location = nullptr;
+
   Planet m_planet;
   int m_x;
   int m_y;
@@ -181,6 +193,8 @@ public:
   Location(int garrison_id) : m_type { Garrison }, m_garrison_id { garrison_id } {}
 
   Location(bc_Location* location) {
+    log_error(location, "Null bc_Location!");
+
     if (bc_Location_is_on_map(location)) {
       m_type = Map;
       m_map_location = bc_Location_map_location(location);
@@ -315,6 +329,7 @@ VEC(UnitType, bc_VecUnitType)
 class Unit {
 public:
   Unit(bc_Unit* unit) : m_unit { unit } {
+    log_error(unit, "Null bc_Unit!");
     m_unittype = bc_Unit_unit_type(unit);
   }
 
@@ -421,6 +436,8 @@ public:
   PlanetMap() {}
 
   PlanetMap(bc_PlanetMap* planet_map) : m_planet_map { planet_map } {
+    log_error(planet_map, "Null bc_PlanetMap!");
+
     m_planet = bc_PlanetMap_planet_get(m_planet_map);
     m_height = bc_PlanetMap_height_get(m_planet_map);
     m_width  = bc_PlanetMap_width_get (m_planet_map);
@@ -452,22 +469,12 @@ public:
 
   bool is_passable_terrain_at(const MapLocation& map_location) const {
     log_error(m_planet_map, "PlanetMap not loaded!");
-
-    auto loc = map_location.to_bc();
-    auto ans = bc_PlanetMap_is_passable_terrain_at(m_planet_map, loc);
-    delete_bc_MapLocation(loc);
-
-    return ans;
+    return bc_PlanetMap_is_passable_terrain_at(m_planet_map, map_location.to_bc());
   }
 
   unsigned get_initial_karbonite_at(const MapLocation& map_location) const {
     log_error(m_planet_map, "PlanetMap not loaded!");
-
-    auto loc = map_location.to_bc();
-    auto ans = bc_PlanetMap_initial_karbonite_at(m_planet_map, loc);
-    delete_bc_MapLocation(loc);
-
-    return ans;
+    return bc_PlanetMap_initial_karbonite_at(m_planet_map, map_location.to_bc());
   }
 
   // TODO: PlanetMap to_string
@@ -494,6 +501,8 @@ public:
   {}
 
   AsteroidStrike(bc_AsteroidStrike* asteroid) {
+    log_error(asteroid, "Null bc_AsteroidStrike!");
+
     m_karbonite = bc_AsteroidStrike_karbonite_get(asteroid);
     m_location = MapLocation(bc_AsteroidStrike_location_get(asteroid));
     delete_bc_AsteroidStrike(asteroid);
@@ -565,6 +574,8 @@ private:
 
 
 // GameMap
+// XXX: This is not used! Moved GameController
+/*
 class GameMap {
 public:
   GameMap(bc_GameMap* game_map) :
@@ -588,13 +599,18 @@ private:
   AsteroidPattern m_asteroid_pattern;
   OrbitPattern    m_orbit_pattern;
 };
+*/
 
 
 //ResearchInfo
 class ResearchInfo{
-  ResearchInfo(bc_ResearchInfo* info) : m_info { info } {}
+  ResearchInfo(bc_ResearchInfo* info) : m_info { info } {
+    log_error(info, "Null bc_ResearchInfo!");
+  }
+
   ~ResearchInfo(){
-     delete_bc_ResearchInfo( m_info );
+    if (m_info)
+      delete_bc_ResearchInfo( m_info );
   }
 
   // These two methods are stand-alone ones from C API, so we decided to
@@ -623,21 +639,300 @@ private:
 //
 
 // GameController
-// Don't instantiate twice, might fail creating multiple bc_GameController
+// IMPORTANT: Don't instantiate twice, might fail creating multiple bc_GameController
 class GameController {
 public:
-  // new GameController failing? shouldn't ever!
-  GameController() : m_gc { new_bc_GameController() }
+  GameController() :
+      m_gc { new_bc_GameController() },
+      m_earth_map { bc_GameController_get_starting_map(m_gc, Earth) },
+      m_mars_map { bc_GameController_get_starting_map(m_gc, Mars) },
+      m_asteroid_pattern { bc_GameController_asteroids_pattern(m_gc) },
+      m_orbit_pattern { bc_GameController_orbit_pattern(m_gc) }
   {}
 
-  inline uint32_t round() const { return bc_GameController_round(m_gc); }
-  inline VecUnit my_units() const {
-    VecUnit units;
+  void next_turn() const { bc_GameController_next_turn(m_gc); }
+  unsigned get_round() const { return bc_GameController_round(m_gc); }
+  Planet get_planet() const { return bc_GameController_planet(m_gc); }
+  Team get_team() const { return bc_GameController_team(m_gc); }
+
+  const PlanetMap& get_starting_planet(Planet planet) {
+    if (planet == Earth) return m_earth_map;
+    return m_mars_map;
   }
 
-//private:
+  // Not in C API
+  const PlanetMap& get_earth_map() const { return m_earth_map; }
+  const PlanetMap& get_mars_map()  const { return m_mars_map; }
+
+  unsigned get_karbonite() const { return bc_GameController_karbonite(m_gc); }
+
+  Unit get_unit(unsigned id) const {
+    bc_Unit* unit = bc_GameController_unit(m_gc, id);
+    log_error(!bc_has_err(), "Incorrect Unit ID!");
+
+    return Unit { unit };
+  }
+
+  std::vector<Unit> get_units() const { return to_vector(bc_GameController_units(m_gc)); }
+  std::vector<Unit> get_my_units() const { return to_vector(bc_GameController_my_units(m_gc)); }
+  std::vector<Unit> get_units_in_space() const { return to_vector(bc_GameController_units_in_space(m_gc)); }
+
+  unsigned get_karbonite_at(MapLocation map_location) const {
+    unsigned val = bc_GameController_karbonite_at(m_gc, map_location.to_bc());
+    log_error(!bc_has_err(), "?");
+
+    return val;
+  }
+
+  std::vector<MapLocation> get_all_locations_within(MapLocation map_location, unsigned radius_squared) const {
+    return to_vector(bc_GameController_all_locations_within(m_gc, map_location.to_bc(), radius_squared));
+  }
+
+  bool can_sense_location(MapLocation map_location) const {
+    return bc_GameController_can_sense_location(m_gc, map_location.to_bc());
+  }
+
+  bool can_sense_unit(unsigned id) const { return bc_GameController_can_sense_unit(m_gc, id); }
+
+  std::vector<Unit> sense_nearby_units(MapLocation map_location, unsigned radius_squared) const {
+    return to_vector(bc_GameController_sense_nearby_units(m_gc, map_location.to_bc(), radius_squared));
+  }
+
+  std::vector<Unit> sense_nearby_units_by_team(MapLocation map_location, unsigned radius_squared, Team team) const {
+    return to_vector(bc_GameController_sense_nearby_units_by_team(m_gc, map_location.to_bc(), radius_squared, team));
+  }
+
+  std::vector<Unit> sense_nearby_units_by_type(MapLocation map_location, unsigned radius_squared, UnitType type) const {
+    return to_vector(bc_GameController_sense_nearby_units_by_type(m_gc, map_location.to_bc(), radius_squared, type));
+  }
+
+  bool has_unit_at_location(MapLocation map_location) const {
+    return bc_GameController_has_unit_at_location(m_gc, map_location.to_bc());
+  }
+
+  // XXX: Only use if after has_unit_at_location! Might crash if not
+  Unit sense_unit_at_location(MapLocation map_location) const {
+    return bc_GameController_sense_unit_at_location(m_gc, map_location.to_bc());
+  }
+
+  const AsteroidPattern& get_asteroid_pattern() const { return m_asteroid_pattern; }
+  const OrbitPattern& get_orbit_pattern()       const { return m_orbit_pattern; }
+
+  unsigned get_current_duration_of_flight() const {
+    return bc_GameController_current_duration_of_flight(m_gc);
+  }
+
+  std::vector<int> get_team_array(Planet planet) const {
+    return to_vector(bc_GameController_get_team_array(m_gc, planet));
+  }
+
+  void write_team_array(unsigned index, int value) const {
+    bc_GameController_write_team_array(m_gc, index, value);
+    log_error(!has_bc_err(), "ArrayOutOfBounds");
+  }
+
+  void disintegrate_unit(unsigned id) const {
+    bc_GameController_disintegrate_unit(m_gc, id);
+    log_error(!has_bc_err(), "?");
+  }
+
+  bool is_occupiable(MapLocation map_location) const {
+    bool ans = bc_GameController_is_occupiable(m_gc, map_location.to_gc());
+    log_error(!has_bc_err(), "?");
+    return ans;
+  }
+
+  bool can_move(unsigned id, Direction direction) const {
+    return bc_GameController_can_move(m_gc, id, direction);
+  }
+
+  bool is_move_ready(unsigned id) const {
+    return bc_GameController_is_move_ready(m_gc, id);
+  }
+
+  void move_robot(unsigned id, Direction direction) const {
+    bc_GameController_move_robot(id, direction);
+    log_error(!has_bc_err(), "?");
+  }
+
+  bool can_attack(unsigned id, unsigned target_id) const {
+    return bc_GameController_can_attack(m_gc, id, target_id);
+  }
+
+  bool is_attack_ready(unsigned id) const {
+    return bc_GameController_is_attack_ready(m_gc, id);
+  }
+
+  void attack(unsigned id, unsigned target_id) const {
+    bc_GameController_attack(id, target_id);
+    log_error(!has_bc_err(), "?");
+  }
+
+  ResearchInfo get_research_info() const {
+    return ResearchInfo { bc_GameController_research_info(m_gc) };
+  }
+
+  bool reset_research() const {
+    return bc_GameController_reset_research(m_gc);
+  }
+
+  bool queue_research(UnitType branch) const {
+    return bc_GameController_queue_research(m_gc, branch);
+  }
+
+  bool can_harvest(unsigned id, Direction direction) const {
+    return bc_GameController_can_harvest(m_gc, id, direction);
+  }
+
+  void harvest(unsigned id, Direction direction) const {
+    bc_GameController_harvest(m_gc, id, direction);
+    log_error(!has_bc_err(), "?");
+  }
+
+  bool can_blueprint(unsigned id, UnitType unittype, Direction direction) const {
+    return bc_GameController_can_blueprint(m_gc, id, unittype, direction);
+  }
+
+  void blueprint(unsigned id, UnitType unittype, Direction direction) const {
+    bc_GameController_blueprint(m_gc, id, unittype, direction);
+    log_error(!has_bc_err(), "?");
+  }
+
+  bool can_build(unsigned worker_id, unsigned blueprint_id) const {
+    return bc_GameController_can_build(m_gc, worker_id, blueprint_id);
+  }
+
+  void build(unsigned worker_id, unsigned blueprint_id) const {
+    bc_GameController_build(m_gc, worker_id, blueprint_id);
+    log_error(!has_bc_err(), "?");
+  }
+
+  bool can_repair(unsigned worker_id, unsigned structure_id) const {
+    return bc_GameController_can_repair(m_gc, worker_id, structure_id);
+  }
+
+  void repair(unsigned worker_id, unsigned structure_id) const {
+    bc_GameController_repair(m_gc, worker_id, structure_id);
+    log_error(!has_bc_err(), "?");
+  }
+
+  void replicate(unsigned worker_id, Direction direction) const {
+    bc_GameController_replicate(m_gc, worker_id, direction);
+    log_error(!has_bc_err(), "?");
+  }
+
+  bool can_javelin(unsigned knight_id, unsigned target_id) const {
+    return bc_GameController_can_javelin(m_gc, knight_id, target_id);
+  }
+
+  bool is_javelin_ready(unsigned knight_id) const {
+    return bc_GameController_is_javelin_ready(m_gc, knight_id);
+  }
+
+  void javelin(unsigned knight_id, unsigned target_id) const {
+    bc_GameController_javelin(m_gc, knight_id, target_id);
+    log_error(!has_bc_err(), "?");
+  }
+
+  bool can_begin_snipe(unsigned ranger_id, MapLocation map_location) const {
+    return bc_GameController_can_begin_snipe(m_gc, ranger_id, map_location.to_bc());
+  }
+
+  void begin_snipe(unsigned ranger, MapLocation map_location) const {
+    bc_GameController_begin_snipe(m_gc, ranger_id, map_location.to_bc());
+    log_error(!has_bc_err(), "?");
+  }
+
+  bool can_begin_blink(unsigned mage_id, MapLocation map_location) const {
+    return bc_GameController_can_begin_blink(m_gc, mage_id, map_location.to_bc());
+  }
+
+  bool is_blink_ready(unsigned mage_id) const {
+    return bc_GameController_is_blink_ready(m_gc, mage_id);
+  }
+
+  void blink(unsigned mage_id, MapLocation map_location) const {
+    bc_GameController_blink(m_gc, mage_id, map_location.to_bc());
+    log_error(!has_bc_err(), "?");
+  }
+
+  bool can_heal(unsigned healer_id, unsigned target_id) const {
+    return bc_GameController_can_heal(m_gc, healer_id, target_id);
+  }
+
+  bool is_heal_ready(unsigned healer_id) const {
+    return bc_GameController_is_heal_ready(m_gc, healer_id);
+  }
+
+  void heal(unsigned healer_id, unsigned target_id) const {
+    bc_GameController_heal(m_gc, healer_id, target_id);
+    log_error(!has_bc_err(), "?");
+  }
+
+  bool is_overcharge_ready(unsigned healer_id) const {
+    return bc_GameController_is_overcharge_ready(m_gc, healer_id);
+  }
+
+  void overcharge(unsigned healer_id, unsigned target_id) const {
+    bc_GameController_overcharge(m_gc, healer_id, target_id);
+    log_error(!has_bc_err(), "?");
+  }
+
+  bool is_load_ready(unsigned structure_id, unsigned robot_id) const {
+    return bc_GameController_is_load_ready(m_gc, structure_id, robot_id);
+  }
+
+  void load(unsigned structure_id, unsigned robot_id) const {
+    bc_GameController_load(m_gc, structure_id, robot_id);
+    log_error(!has_bc_err(), "?");
+  }
+
+  bool is_unload_ready(unsigned structure_id, Direction direction) const {
+    return bc_GameController_is_unload_ready(m_gc, structure_id, direction);
+  }
+
+  void unload(unsigned structure_id, Direction direction) const {
+    bc_GameController_unload(m_gc, structure_id, direction);
+    log_error(!has_bc_err(), "?");
+  }
+
+  bool can_produce_robot(unsigned factory_id, UnitType unittype) const {
+    return bc_GameController_can_produce_robot(m_gc, factory_id, unittype);
+  }
+
+  void produce_robot(unsigned factory_id, UnitType unittype) const {
+    bc_GameController_produce_robot(m_gc, factory_id, unittype);
+    log_error(!has_bc_err(), "?");
+  }
+
+  RocketLandingInfo get_rocket_landings() const {
+    return RocketLandingInfo { bc_GameController_rocket_landings(m_gc); };
+  }
+
+  bool can_launch_rocket(unsigned rocket_id, MapLocation map_location) const {
+    return bc_GameController_can_launch_rocket(m_gc, rocket_id, map_location.to_bc());
+  }
+
+  void launch_rocket(unsigned rocket_id, MapLocation map_location) const {
+    bc_GameController_launch_rocket(m_gc, rocket_id, map_location.to_bc());
+    log_error(!has_bc_err(), "?");
+  }
+
+  bool is_over() const {
+    return bc_GameController_is_over(m_gc);
+  }
+
+  Team get_winning_team() const {
+    return bc_GameController_winning_team(m_gc);
+  }
+
+private:
   bc_GameController* m_gc;
 
+  PlanetMap       m_earth_map;
+  PlanetMap       m_mars_map;
+  AsteroidPattern m_asteroid_pattern;
+  OrbitPattern    m_orbit_pattern;
 };
 
 
