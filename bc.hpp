9caa98f7f55c79e9946594df1fa6c1ce2d38f0ec
Naum Azeredo <naumazeredo@gmail.com>
2018-01-09 22:37:23 -0300

    Sending incomplete stuff to sync

--- bc.hpp
+++ bc.hpp
@@ -7,22 +7,26 @@
 
 // XXX: Update when C API is released (using Python as reference)
 
+#include <vector>
+
 #include <bc.h>
+
 
 namespace bc {
 
 // TODO: Implement the classes/namespaces/everything using C++11/C++14/C++17
+// TODO: Order classes correctly
 
-// AsteroidPattern
-// AsteroidStrike
-// Delta (?)
+// Planet
+using Planet = bc_Planet;
+
 
 // Direction
 using Direction = bc_Direction;
 
 inline Direction opposite_direction(Direction d) { return bc_Direction_opposite(d); }
 
-// ErrorMessage (?)
+
 
 // GameController
 // Don't instantiate twice, might fail creating multiple bc_GameController
@@ -32,38 +36,52 @@ public:
   GameController() : m_gc { new_bc_GameController() }
   {}
 
-  inline uint32_t get_round() const { return bc_GameController_round(m_gc); }
+  inline uint32_t round() const { return bc_GameController_round(m_gc); }
+  inline VecUnit my_units() const {
+    VecUnit units;
+  }
 
 //private:
   bc_GameController* m_gc;
 
-}
+};
 
-// GameMap
-// InitialTurnApplication (?)
-// Location
-// MapLocation
-// OrbitPattern
-// Planet
-// PlanetMap
-// Player (?)
-// ResearchInfo
-// RocketLanding
-// RocketLandingInfo
-// StartGameMessage (?)
-// StartTurnMessage (?)
-// Team
-// TurnApplication (?)
-// TurnMessage (?)
+
+
 // Unit
-// UnitType
-// VecMapLocation
-// VecRocketLanding
+class Unit {
+public:
+  Unit(bc_Unit* unit) : m_unit { unit }, m_id { 0 }
+  {}
+
+  inline uint16_t get_id() {
+    if (m_id) return m_id;
+    return m_id = bc_Unit_id(m_unit);
+  }
+
+//private:
+  bc_Unit* m_unit;
+  uint16_t m_id;
+};
+
+
+
 // VecUnit
-// VecUnitID
-// VecUnitType
-// Veci32
-// ViewerKeyframe (?)
-// ViewerMessage (?)
+class VecUnit {
+public:
+  VecUnit() : m_vec { nullptr }
+  {}
+
+  operator =(
+
+  ~VecUnit() {
+    if (m_vec)
+      delete m_vec;
+  }
+
+//private;
+  bc_VecUnit* m_vec;
+};
+
 
 }
