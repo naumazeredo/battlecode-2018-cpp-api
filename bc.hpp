362189aac8107cf69eae061c98ec890e3341309d
Naum Azeredo <naumazeredo@gmail.com>
2018-01-12 02:11:37 -0300

    Add check for GameControlle constructor

--- bc.hpp
+++ bc.hpp
@@ -889,7 +889,9 @@ public:
       m_mars_map { bc_GameController_starting_map(m_gc, Mars) },
       m_asteroid_pattern { bc_GameController_asteroid_pattern(m_gc) },
       m_orbit_pattern { bc_GameController_orbit_pattern(m_gc) }
-  {}
+  {
+    CHECK_ERRORS();
+  }
 
   ~GameController() {
     if (m_gc)
